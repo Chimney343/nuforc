073c25af1d40d2609a102362ef99c4d2971f3eab
Chimney343 <mk.kominek@gmail.com>
2022-01-02 17:55:05 +0100

    Configured the scraper to run with SETTINGS.py

--- run.py
+++ run.py
@@ -3,11 +3,19 @@ import logging.config
 from model.modules.scraping import NUFORCScraper
 
 logging.config.dictConfig(SETTINGS.LOGGER_CONFIG)
-logger = logging.getLogger(__name__)
+logger = logging.getLogger("root")
+
 
 def execute_scraping():
-    scraper = NUFORCScraper(scraping_mode=SETTINGS.SCRAPING_MODE, timespan_start=SETTINGS.TIMESPAN_START, timespan_end=SETTINGS.TIMESPAN_END, output_folder=SETTINGS.OUTPUT_FOLDER)
+    scraper = NUFORCScraper(
+        scraping_mode=SETTINGS.SCRAPING_MODE,
+        timespan_start=SETTINGS.TIMESPAN_START,
+        timespan_end=SETTINGS.TIMESPAN_END,
+        n_scraping_retries=SETTINGS.N_SCRAPING_RETRIES,
+        output_folder=SETTINGS.OUTPUT_FOLDER,
+    )
     scraper.scrape()
+
 
 if __name__ == "__main__":
     execute_scraping()
