57b1f94a54c69218a0d215a036b4c7b09d44c422
Chimney <Sshfail11%>
2022-03-01 00:16:37 +0100

    wrangling.py - wrangling overhaul into NUFORCReportProcessor class performing all the hard work on extracting data from NUFORC reports.

--- model/modules/wrangling.py
+++ model/modules/wrangling.py
@@ -15,82 +15,137 @@ import logging
 logger = logging.getLogger("model.modules.wrangling")
 
 """
-RAW EVENT TEXT ANALYSIS
-A collections of functions used to analyze the raw text of each NUFORC event page. Most are regex based.
+RAW REPORT ANALYSIS
 """
+class NUFORCReportProcessor:
+    def __init__(self, raw_report):
+        self.raw_report = raw_report
+        self.is_raw_report_correct = self._validate_raw_report(raw_report)
 
+    def _validate_raw_report(self, raw_report):
+        failed_raw_report_indicators = [
+            "Unable to download report",
+            "Blank report",
+            "Unable to parse BeautifulSoup from downloaded page.",
+        ]
+        if not isinstance(raw_report, str):
+            return False
+        if any(
+                indicator in raw_report for indicator in failed_raw_report_indicators
+        ):
+            return False
 
-def extract_description(raw_event_text):
+    def process(self, raw_report=None):
+        if raw_report is None:
+            raw_report=self.raw_report
+
+        self.url = extract_url(raw_report=raw_report)
+        self.occurred_time = extract_time(
+            raw_report=raw_report, time_type="occurred_time"
+        )
+        self.reported_time = extract_time(
+            raw_report=raw_report, time_type="reported_time"
+        )
+        self.entered_as_time = extract_time(
+            raw_report=raw_report, time_type="entered_as_time"
+        )
+        self.shape = extract_shape(raw_report=raw_report)
+        self.duration = extract_duration(raw_report=raw_report)
+        self.city = extract_city(raw_report=raw_report)
+        self.state = extract_state(raw_report=raw_report)
+        self.state_abbreviation = extract_state_abbreviation(
+            raw_report=raw_report
+        )
+        self.country = extract_country(raw_report=raw_report)
+        self.description = extract_description(raw_report=raw_report)
+        
+    def get_report(self):
+        self.process()
+        
+        return {"raw_report": self.raw_report,
+                "url": self.url,
+                "occurred_time": self.occurred_time,
+                "reported_time": self.reported_time,
+                "entered_as_time": self.entered_as_time,
+                "shape": self.shape,
+                "duration": self.duration,
+                "city": self.city,
+                "state": self.state,
+                "state_abbreviation": self.state_abbreviation,
+                "country": self.country,
+                "description": self.description}
+    
+def extract_description(raw_report):
     try:
-        info = raw_event_text.splitlines()[-1].strip()
+        info = "".join(raw_report.splitlines()[2:]).strip()
         return info
     except:
-        return "not parsed"
+        return "unparsed"
 
 
-def extract_url(raw_event_text):
+def extract_url(raw_report):
     try:
-        info = raw_event_text.splitlines()[0].strip()
+        info = raw_report.splitlines()[0].strip()
         return info
     except:
-        return "not parsed"
+        return "unparsed"
 
 
-def extract_city(raw_event_text):
-    location_match = REGEX_DICT["location"].search(raw_event_text)
+def extract_city(raw_report):
+    location_match = REGEX_DICT["location"].search(raw_report)
     if location_match is not None:
         location_match = location_match.group()
         try:
             info = get_city_from_location(location_match)
             return info
         except:
-            return "not parsed"
+            return "unparsed"
 
 
-def extract_state_abbreviation(raw_event_text):
-    location_match = REGEX_DICT["location"].search(raw_event_text)
+def extract_state_abbreviation(raw_report):
+    location_match = REGEX_DICT["location"].search(raw_report)
     if location_match is not None:
         location_match = location_match.group()
         try:
             info = get_state_info(location_match)["state_abbreviation"]
             return info
         except:
-            return "not parsed"
+            return "unparsed"
 
 
-def extract_state(raw_event_text):
-    location_match = REGEX_DICT["location"].search(raw_event_text)
+def extract_state(raw_report):
+    location_match = REGEX_DICT["location"].search(raw_report)
     if location_match is not None:
         location_match = location_match.group()
         try:
             info = get_state_info(location_match)["state"].name
             return info
         except:
-            return "not parsed"
+            return "unparsed"
 
 
-def extract_country(raw_event_text):
-    location_match = REGEX_DICT["location"].search(raw_event_text)
+def extract_country(raw_report):
+    location_match = REGEX_DICT["location"].search(raw_report)
     if location_match is not None:
         location_match = location_match.group()
         try:
             info = get_country_from_location(location_match)
             return info
         except:
-            return "not parsed"
+            return "unparsed"
 
 
-def extract_duration(raw_event_text):
-    match = REGEX_DICT["duration"].search(raw_event_text)
+def extract_duration(raw_report):
+    match = REGEX_DICT["duration"].search(raw_report)
     if match is not None:
         time_string = match.group()
         return parse_duration(time_string)
     else:
-        return "not parsed"
+        return "unparsed"
 
 
-def extract_time(raw_event_text, time_type):
-    match = REGEX_DICT[time_type].findall(raw_event_text)
+def extract_time(raw_report, time_type):
+    match = REGEX_DICT[time_type].findall(raw_report)
     if match is not None:
         try:
             match = [s for s in match[0] if s != ""][1:]
@@ -99,69 +154,16 @@ def extract_time(raw_event_text, time_type):
             event_datetime = " ".join([event_date, event_time])
             return event_datetime
         except:
-            return "not parsed"
+            return "unparsed"
 
 
-def extract_shape(raw_event_text):
-    match = REGEX_DICT["shape"].search(raw_event_text)
+def extract_shape(raw_report):
+    match = REGEX_DICT["shape"].search(raw_report)
     if match is not None:
         info = match.group()
         return info
     else:
-        return "not parsed"
-
-
-def make_event_summary(raw_event_text):
-    keys = [
-        "raw_event_text",
-        "url",
-        "occurred_time",
-        "reported_time",
-        "entered_as_time",
-        "shape",
-        "duration",
-        "city",
-        "state",
-        "state_abbreviation",
-        "country",
-        "description",
-    ]
-    summary = dict.fromkeys(keys, "not parsed")
-
-    if not isinstance(raw_event_text, str):
-        return summary
-
-    failed_raw_event_text_indicators = [
-        "Unable to download event page.",
-        "Blank event page.",
-        "Unable to parse BeautifulSoup from downloaded page.",
-    ]
-    if any(
-        indicator in raw_event_text for indicator in failed_raw_event_text_indicators
-    ):
-        return summary
-
-    summary["raw_event_text"] = raw_event_text
-    summary["url"] = extract_url(raw_event_text=raw_event_text)
-    summary["occurred_time"] = extract_time(
-        raw_event_text=raw_event_text, time_type="occurred_time"
-    )
-    summary["reported_time"] = extract_time(
-        raw_event_text=raw_event_text, time_type="reported_time"
-    )
-    summary["entered_as_time"] = extract_time(
-        raw_event_text=raw_event_text, time_type="entered_as_time"
-    )
-    summary["shape"] = extract_shape(raw_event_text=raw_event_text)
-    summary["duration"] = extract_duration(raw_event_text=raw_event_text)
-    summary["city"] = extract_city(raw_event_text=raw_event_text)
-    summary["state"] = extract_state(raw_event_text=raw_event_text)
-    summary["state_abbreviation"] = extract_state_abbreviation(
-        raw_event_text=raw_event_text
-    )
-    summary["country"] = extract_country(raw_event_text=raw_event_text)
-    summary["description"] = extract_description(raw_event_text=raw_event_text)
-    return summary
+        return "unparsed"
 
 
 def get_state_info(location):
@@ -199,7 +201,6 @@ def get_state_info(location):
                 "country": "Canada",
             }
             return state_info
-
 
 def get_valid_country_name(name, custom_lookup=NON_ISO_3166_COUNTRY_NAMES):
     """
