56b8fb2d2925fde93d24d605979bc25e54ab3a06
Chimney <Sshfail11%>
2022-03-16 23:12:09 +0100

    Settings adjustments.

--- SETTINGS.py
+++ SETTINGS.py
@@ -1,4 +1,5 @@
 import os
+
 from dotenv import load_dotenv
 
 load_dotenv()  # take environment variables from .env.
@@ -6,26 +7,24 @@ load_dotenv()  # take environment variables from .env.
 """
 Scraper settings.
 """
-SCRAPING_MODE = 'full'
-TIMESPAN_START = "2021-01-01"
-TIMESPAN_END = "2021-01-10"
+SCRAPING_MODE = 'timespan'
+TIMESPAN_START = "1900-01"
+TIMESPAN_END = "2030-01"
 N_SCRAPING_RETRIES = 20
 OUTPUT_FOLDER = os.getenv("OUTPUT_FOLDER")
 
 """
 API keys.
 """
-GOOGLE_API_KEY=os.getenv("GOOGLE_API_KEY")
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 
 """
 Logger settings.
 """
 
-LOGGER_CONFIG = {
+LOGGING_CONFIG = {
     "version": 1,
-    "formatters": {
-        "simple": {"format": "%(asctime)s - %(name)s - %(levelname)s - %(message)s"}
-    },
+    "formatters": {"simple": {"format": "%(asctime)s - %(name)s - %(levelname)s - %(message)s"}},
     "handlers": {
         "console": {
             "class": "logging.StreamHandler",
