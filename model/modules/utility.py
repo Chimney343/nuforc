87812c2364811da53817cb712d700fb02c0de936
Chimney <Sshfail11%>
2022-03-16 23:10:39 +0100

    Simplified utilities in accordance to scraping.py overhaul.

--- model/modules/utility.py
+++ model/modules/utility.py
@@ -1,11 +1,14 @@
-import requests
-from bs4 import BeautifulSoup
-from requests.exceptions import ConnectionError, ConnectTimeout, ReadTimeout
+import logging
 from datetime import datetime
 from urllib.parse import urljoin
 
-import logging
+import requests
+from bs4 import BeautifulSoup
+from dateutil.parser import parse
+from requests.exceptions import ConnectionError, ConnectTimeout, ReadTimeout
+
 logger = logging.getLogger("model.modules.utility")
+
 
 def get_page(url, n_scraping_retries=10, page_type=''):
     attempt = 0
@@ -25,38 +28,43 @@ def get_page(url, n_scraping_retries=10, page_type=''):
                 return None
 
 
-def make_monthly_event_summary_lookup(n_scraping_retries=10):
+def make_month_root_lookup(n_scraping_retries=10):
     url = "http://www.nuforc.org/webreports/ndxevent.html"
-    page = get_page(url=url, n_scraping_retries=n_scraping_retries, page_type='Monthly event summary root page')
-    if page is not None:
+    page = get_page(url=url, n_scraping_retries=n_scraping_retries, page_type='Month page root lookup')
+    if page.status_code == 200:
         try:
             soup = BeautifulSoup(page.text, "html.parser")
             monthly_event_summary_tag_list = list(soup.find_all("a", href=True))[1:-1]
-
             lookup = {}
             for tag in monthly_event_summary_tag_list:
                 month = datetime.strptime(tag['href'][4:10], '%Y%m')
-
                 monthly_event_summary_url = urljoin("http://www.nuforc.org/webreports/", tag["href"])
                 lookup[month] = monthly_event_summary_url
             return lookup
         except:
             return None
     else:
-        raise ConnectionError("Unable to connect with NUFORC monthly event summary page.")
+        raise ConnectionError(
+            f"Unable to connect with NUFORC monthly event summary page after {n_scraping_retries} retries."
+        )
 
-def validate_datetime_format(date):
+
+def is_date(string, fuzzy=False):
+    """
+    Return whether the string can be interpreted as a date.
+
+    :param string: str, string to check for date
+    :param fuzzy: bool, ignore unknown tokens in string if True
+    """
     try:
-        date = datetime.strptime(date, "%Y-%m-%d")
+        parse(string, fuzzy=fuzzy)
+        return True
+
     except ValueError:
-        raise ValueError(
-            f"Datetime - > {date} < -  does not match format '%Y-%m-%d' (i.e '2012-12-12', '2016-04-01)"
-            )
-    return date
+        return False
+
 
 def last_day_of_month(date):
     if date.month == 12:
         return date.replace(day=31)
-    return date.replace(month=date.month+1, day=1) - datetime.timedelta(days=1)
-
-
+    return date.replace(month=date.month + 1, day=1) - datetime.timedelta(days=1)
